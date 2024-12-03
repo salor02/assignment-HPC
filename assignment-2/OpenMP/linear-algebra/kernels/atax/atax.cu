94d870af75c3a0ce180699460e15430893c3ce59
lucadileo9 <lucadileo70@gmail.com>
2024-12-03 16:52:14 +0100

    style: change some names

--- assignment-2/OpenMP/linear-algebra/kernels/atax/atax.cu
+++ assignment-2/OpenMP/linear-algebra/kernels/atax/atax.cu
@@ -53,33 +53,31 @@ static void print_array(int nx,
 
 
 
-__global__ void atax_kernel1(int nx, int ny, DATA_TYPE *A, DATA_TYPE *x, DATA_TYPE *tmp)
+__global__ void compute_tmp(int nx, int ny, DATA_TYPE *A, DATA_TYPE *x, DATA_TYPE *tmp)
 {
-	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	int row = blockIdx.x * blockDim.x + threadIdx.x;
 
-	if (i < _PB_NX)
+	if (row < _PB_NX)
 	{
-		tmp[i] = 0;
-		int j;
-		for(j=0; j < _PB_NY; j++)
-		{
-			tmp[i] += A[i*NY+j] * x[j];
-		}
+        float sum = 0.0;
+        for (int j = 0; j < NY; j++) {
+            sum += A[row * NY + j] * x[j];
+        }
+        tmp[row] = sum;
 	}
 }
 
-__global__ void atax_kernel2(int nx, int ny, DATA_TYPE *A, DATA_TYPE *y, DATA_TYPE *tmp)
+__global__ void compute_y(int nx, int ny, DATA_TYPE *A, DATA_TYPE *y, DATA_TYPE *tmp)
 {
-	int j = blockIdx.x * blockDim.x + threadIdx.x;
+	int col = blockIdx.x * blockDim.x + threadIdx.x;
 	
-	if (j < _PB_NY)
+	if (col < _PB_NY)
 	{
-		y[j] = 0;
-		int i;
-		for(i=0; i < _PB_NX; i++)
-		{
-			y[j] += A[i*NY+j] * tmp[i];
-		}
+        float sum = 0.0;
+        for (int i = 0; i < NX; i++) {
+            sum += A[i * NY + col] * tmp[i];
+        }
+        y[col] = sum; // Accumula i risultati in parallelo
 	}
 }
 
